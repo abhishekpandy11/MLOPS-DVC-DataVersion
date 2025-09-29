b0a40aa5782a28c8cf26945ffabc1825cdb8f536
Abhishek <abhishekpandy25@gmail.com>
2025-09-29 15:14:48 +0530

    first commit before dvc

---
+++ mycode.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import os 
+
+# create a sample dataframe with columns names
+data = {'Name':['Alice','Bob','Charlie'],
+        'Age':[25,30,35],
+        'CIty':['New York','Los Angles','Chicago']
+        }
+
+df = pd.DataFrame(data)
+
+# Adding new row to df for v2
+# 
+# Ensure the data directory exists at the root level
+Data_dir = 'data'
+os.makedirs(Data_dir,exist_ok=True)
+# Define the file path
+file_path = os.path.join(Data_dir,'sample_data.csv')
+df.to_csv(file_path,index=False)
+print(f'csv file saved to {file_path}')
